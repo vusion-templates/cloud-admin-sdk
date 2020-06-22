fb4e2f2f91290e3ff1b7cb4e0932d17dc212778a
Forrest <rainforest92@126.com>
2020-06-22 16:23:50 +0800

    :sparkles: Add auth option in app.config.js

--- template/page/src/app.config.js
+++ template/page/src/app.config.js
@@ -2,8 +2,10 @@ export default {
     title: '{{ title }}',
     name: '{{ name }}',
     layout: '{{ layout }}',
+    auth: {{ auth }},
     router: {
         defaults: '/overview',
         notFound: '/overview',
+        unauthorized: '/overview',
     },
 };
